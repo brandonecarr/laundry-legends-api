a7f3f6008587a5fe4be8b70ddb95916b92372f2a
brandonecarr <brandonecarr@gmail.com>
2025-12-29 12:29:57 -0800

    Phase 3: Add payments & subscriptions backend

--- app/controllers/api/v1/subscription_plans_controller.rb
+++ app/controllers/api/v1/subscription_plans_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
       class Api::V1::SubscriptionPlansController < ApplicationController
+          before_action :authenticate_request
         # Remove the skip_before_action line entirely
         
         def index
